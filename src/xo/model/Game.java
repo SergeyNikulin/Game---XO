07205c4f92841427643af510006f0bb9c4f1c1ef
Sergey Nikulin <nikulin-91@mail.ru>
2018-04-19 16:24:26 +0400

    Game - implement

--- src/xo/model/Game.java
+++ src/xo/model/Game.java
@@ -2,4 +2,28 @@ package xo.model;
 
 public class Game {
 
+    private final String name;
+
+    private final Player[] players;
+
+    private final Field field;
+
+    public Game(final String name, final Field field, final Player[] players) {
+        this.name = name;
+        this.players = players;
+        this.field = field;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
 }
