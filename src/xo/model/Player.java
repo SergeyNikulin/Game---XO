36b0ec7a222507dc67f650921bd7a6f8fb98f3da
Sergey Nikulin <nikulin-91@mail.ru>
2018-04-19 09:54:38 +0400

    Player - implements

--- src/xo/model/Player.java
+++ src/xo/model/Player.java
@@ -2,4 +2,21 @@ package xo.model;
 
 public class Player {
 
+    private String name;
+
+    private Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
 }
